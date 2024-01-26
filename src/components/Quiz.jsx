81d381066e851aaf0d2953e22c0df3098c311dcc
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-01-26 20:26:04 +0400

    final

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
+import QEUSTIONS from "../questions.js";
+import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QEUSTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectAnswer];
+    });
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+
+  if (quizIsComplete) {
+    return  <Summary userAnswers={userAnswers}/>
+  };
+
   return (
-    <p id="quiz">Questions</p>
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
   );
 }
